2e4166d243d76f9f13f03f2dd90583be1869c160
student <ravi_badam@yahoo.com>
2021-06-19 16:55:11 +0000

    Cool Maven springboot project

---
+++ src/main/java/ravi/com/example/springpipelinedemo/SpringpipelinedemoApplication.java
@@ -0,0 +1,13 @@
+package ravi.com.example.springpipelinedemo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SpringpipelinedemoApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SpringpipelinedemoApplication.class, args);
+	}
+
+}
---
+++ src/test/java/ravi/com/example/springpipelinedemo/SpringpipelinedemoApplicationTests.java
@@ -0,0 +1,13 @@
+package ravi.com.example.springpipelinedemo;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SpringpipelinedemoApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
